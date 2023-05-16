d264923f76332c65dfb03a505d1d3fab09f3e35f
trong.ld <ductronglu0411@gmail.com>
2023-05-16 11:48:59 +0700

    á

--- utils/blacklist.js
+++ utils/blacklist.js
@@ -6,16 +6,13 @@ function addTokenToBlacklist(token) {
     token: token,
     timeLogout: currentTime,
   });
-  console.log(blackList);
 }
 
 function isTokenBlacklisted(token, verify) {
   console.log(token, verify);
   const deathToken = blackList.filter((e) => (e.token = token));
   console.log('death',deathToken);
-  // const currentTime = Math.floor(Date.now() / 1000);
   if (deathToken.length > 0) {
-    console.log(1);
     if (deathToken.timeLogout <= verify.iat) {
       return false;
     } else return true;
